24c390b8b5ae6680047a43ddea5ab5e115979a2f
JoanVal <joan.valverde@ucr.ac.cr>
2019-02-14 22:33:56 -0600

    primer script

---
+++ primer_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Hola, estudiante del taller de Linux."
+
+# Lectura interactiva del nombre
+echo "Escriba su nombre: "
+read name
+
+# Mostrar la variable name
+echo Su nombre es: $name
